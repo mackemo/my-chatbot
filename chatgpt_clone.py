665bbfb5e42975f3386125d1912ad79cf0f49ab3
mackemo <mackenzielmoore14@gmail.com>
2024-09-16 17:44:27 -0400

    Set app title, model, initialize chat history, and display messages

--- chatgpt_clone.py
+++ chatgpt_clone.py
@@ -1,2 +1,21 @@
 import streamlit as st
 from openai import OpenAI
+
+st.title("My-Chatbot")
+
+# Set OpenAI API key from Streamlit secrets
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+# Set a default model
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
